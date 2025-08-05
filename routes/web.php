a063e98bceb2d2fd2b4e9b282f675ac988def6c1
IlyoskhujaSulaymonov <sulaymonovilyos22@gmail.com>
2025-08-05 18:47:03 +0500

    Some changes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,15 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuestionsController;
-
-
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\AdminDashboardController;
+use App\Http\Controllers\Admin\TeacherController;
+use App\Http\Controllers\Admin\StudentController;
+use App\Http\Controllers\Admin\SchoolClassController;
+use App\Http\Controllers\Admin\SubjectController;
+use App\Http\Controllers\Admin\TaskController;
+use App\Http\Controllers\TeacherDashboardController;
+use App\Http\Controllers\StudentDashboardController;
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -18,5 +25,36 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [MainController::class, 'dashboard'])->name('dashboard');
     Route::resource('questions', QuestionsController::class);
 });
+
+Route::get('/admin/dashboard', [AdminDashboardController::class, 'dashboard'])->name('admin.dashboard');
+
+Route::prefix('admin')->middleware('auth')->name('admin.')->group(function () {
+    Route::resource('teachers', TeacherController::class);
+});
+
+Route::prefix('admin')->middleware('auth')->name('admin.')->group(function () {
+    Route::resource('students', StudentController::class);
+});
+
+Route::prefix('admin')->middleware('auth')->name('admin.')->group(function () {
+    Route::resource('classes', SchoolClassController::class);
+});
+
+Route::prefix('admin')->middleware('auth')->name('admin.')->group(function () {
+    Route::resource('subjects', SubjectController::class);
+});
+
+Route::prefix('admin')->middleware('auth')->name('admin.')->group(function () {
+    Route::resource('tasks', TaskController::class);
+});
+
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/teacher/dashboard', [TeacherDashboardController::class, 'index'])->name('teacher.dashboard');
+    Route::get('/student/dashboard', [StudentDashboardController::class, 'index'])->name('student.dashboard');
+});
+
+
 
 require __DIR__.'/auth.php';
